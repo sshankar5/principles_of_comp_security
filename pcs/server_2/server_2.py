d9ab325887c0cafd27fceca6c61c4d31e65cd887
nishi283 <89824772+nishi283@users.noreply.github.com>
2022-05-10 22:59:06 -0400

    Update server_2.py

--- pcs/server_2/server_2.py
+++ pcs/server_2/server_2.py
@@ -1,21 +1,70 @@
+import os
+from socket import *
+from cryptography.fernet import Fernet
+
+c_key = b'Z4-L_1FMlhMiHJgNtI5hCyry2nV6-brcEW2lOsFZ7K8='
+c_Pkey = Fernet(c_key)
+server_ip = "localhost"
+server_port = 8002
+ds_ip = 'localhost'
+ds_port = 9001
+s_sock = socket(AF_INET, SOCK_STREAM)
+s_sock.bind((server_ip, server_port))
+s_sock.listen(5)
+print("Server is ready")
+curr_dir = os.getcwd()
+fv_map = {}
+p_ip = "localhost"
+p_port = 8001
 
 
+def socket_connection():
+    c_sock = socket(AF_INET, SOCK_STREAM)
+    return c_sock
 
 
+def read_write_request(filename, rw, write_data, fv_map, c_id):
+    cs1 = socket_connection()
+    filename1 = filename.split("\\")[-1]
+    size = len(filename1)
+    filename1 = filename1[:size - 4]
+    fn = c_Pkey.decrypt(filename1.encode('utf-8')).decode('utf-8')
+    print("filename sent is ",fn)
+    send_query = fn + "|" + c_id + "|" +"GET_KEY"
+    send_query = c_Pkey.encrypt(send_query.encode('utf-8'))
+    cs1.connect((ds_ip, ds_port))
+    cs1.send(send_query)
+    reply = cs1.recv(1024)
+    reply = c_Pkey.decrypt(reply).decode('utf-8')
+    if reply == "Not permitted":
+        return "You are not permitted"
+    else:
+        u_key = reply
+        u_Pkey = Fernet(u_key)
+    
+    if rw == "a+":  # if write request
+        if filename not in fv_map:
+            fv_map[filename] = 0  # if empty (ie. if its a new file), set the version no. to 0
+        else:
+            fv_map[filename] += 1  # increment version no.
 
+        file = open(filename, "a+")
+        file.write(write_data)
+        print("New version of " + filename + " is " + str(fv_map[filename]))
+        return "write request is successful", fv_map[filename]
 
-
-
-
-
-
-
-
-
-
-
-
-
+    elif rw == "r":  # if read request
+        try:
+            file = open(filename, "r")
+            filedata = file.read()  # read the file's text into a string
+            filedata = u_Pkey.decrypt(filedata.encode('utf-8')).decode('utf-8')
+            print("filedata is ",filedata)
+            if filename not in fv_map:
+                fv_map[filename] = 0
+            return (filedata, fv_map[filename])
+        except IOError:  # IOError occurs when open(filepath,RW) cannot find the file requested
+            print(filename + " does not exist\n")
+            return "File does not exist", -1
 
 def main():
     while 1:
