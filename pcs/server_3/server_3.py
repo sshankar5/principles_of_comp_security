19e2cdbcf68a58ccb63727e31ba0364ee40e247e
saikrishnad27 <105332838+saikrishnad27@users.noreply.github.com>
2022-05-10 23:12:09 -0400

    Update server_3.py

--- pcs/server_3/server_3.py
+++ pcs/server_3/server_3.py
@@ -126,6 +126,9 @@ def client_response(resp, rw, client_socket):
     client_socket.send(reply)
 
 def main():
+    T = Thread(target = thread_1)
+    T.setDaemon(True)                  
+    T.start()       
     while 1:
         client_socket, address = s_sock.accept()
         msg = client_socket.recv(1024)
