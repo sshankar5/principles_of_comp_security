314ff1d75a0c4c7df80a54cc8ccdbe2cd3dfb586
nishi283 <89824772+nishi283@users.noreply.github.com>
2022-05-10 22:56:12 -0400

    Update server.py

--- pcs/server/server.py
+++ pcs/server/server.py
@@ -44,3 +44,83 @@ def getListOfFiles(dirName):
                 allFiles.append(entry)
 
     return allFiles
+
+def server_status_check():
+    print("server health is being checked")
+    cs1 = socket_connection()
+    b = getListOfFiles(curr_dir)
+    c = []
+    for x in b:
+        if x.endswith(".txt"):
+            size = len(x)
+            x1 = x[:size - 4]
+            try:
+                x2 = c_Pkey.decrypt(x1.encode()).decode('utf-8')
+                c.append(x2)
+            except:
+                c.append(x1)
+    send_query = ' '.join([str(item) for item in c])
+    # print(send_query)
+    send_query = "server_status_check" + " " + send_query
+    print("the details of send query:")
+    print(send_query)
+    send_query = c_Pkey.encrypt(send_query.encode('utf-8'))
+    cs1.connect((ds_ip, ds_port))
+    cs1.send(send_query)
+    cs1.close()
+
+def replica_file(filename):
+    data = []
+    print(filename)
+    if "<DIRRENAME>" in filename:
+        h = filename.split('|')[4]
+        dat = eval(h)
+        data=dat['replicas']
+        msg=filename
+    elif "<DIRCREATE>" in filename:
+        h = filename.split('|')[3]
+        dat = eval(h)
+        data=dat['replicas']
+        msg=filename
+    elif "RENAME" in filename:
+        msg = filename
+        h = filename.split('|')[3]
+        data = eval(h)
+    # data = p['replicas']
+    elif "DELETE" in filename:
+        msg = filename
+        h = filename.split('|')[2]
+        data = eval(h)
+        # data = p['replicas']
+    else:
+        print(fv_map)
+        fname = filename.split('|')[0]
+        path=filename.split('|')[1]
+        print("fname in replica ", fname)
+        f = open(fname, 'r')
+        #fn = c_Pkey.decrypt(fn.encode('utf-8')).decode('utf-8')
+        #fn+=".txt"
+        text = f.read()
+        f.close()
+        file1 = filename.split('|')[3]
+        print("fv_map is ",fv_map[fname])
+        msg = "REPLICATE|" + file1 + "|" + text + "|" + str(fv_map[fname]) + "|" + path
+        print("msg is ",msg)
+        h = filename.split('|')[2]
+        data = eval(h)
+
+    for i in data:
+        server_ip = i['server_name']
+        port = i['portno']
+        try:
+            replicate_socket = socket(AF_INET, SOCK_STREAM)
+            replicate_socket.connect((server_ip, port))
+            msg1 = c_Pkey.encrypt(msg.encode('utf-8'))
+            replicate_socket.send(msg1)
+            print("replication done succesfully")
+            replicate_socket.close()
+        except error as msg:
+            print(msg)
+            print("exception occured")
+
+
